94cdbeba937efe76d8108f7c45da4bbb6f351887
praneethkapilavai <praneethkapilavai@gmail.com>
2026-02-15 18:28:32 +0530

    implemented create and read in CRUD

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,9 +1,9 @@
 
-function UserList(){
+function UserList(props){
     return(
         <div>
-            <h1 className="mt-3">List of Users</h1>
-        <table className="table">
+            <h1 className="mt-3 ">List of Users</h1>
+        <table className="table  table-bordered table-striped">
             <thead>
                 <tr>
                    <th>Username</th>
@@ -11,7 +11,16 @@ function UserList(){
                    <th>Email</th>
                 </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+                {props.User.map((user) => (
+                    <tr key={user.id}>
+                        <td >{user.name}</td>
+                        <td >{user.date}</td>
+                        <td >{user.email}</td>
+                        <button className="btn btn-danger btn-sm bg-danger">x</button>
+                    </tr>
+                ))}
+            </tbody>
         </table>
         </div>
             
--- src/components/UserManagement.jsx
+++ src/components/UserManagement.jsx
@@ -1,18 +1,45 @@
 import UserList from './UserList'
 import UserForm from './UserForm'
+import { useState, useEffect} from 'react';
 
 function UserManagement(){
 
+    let [User , setUser]= useState([])
+
+    async function  createUser(newUser){
+        // setUser(newUser)
+        let res = await fetch('http://localhost:3000/users' , {
+            method : "POST",
+            headers : {"Context-Type" : "application/json"},
+            body : JSON.stringify(newUser)  
+        })
+        // console.log(res)
+        // setUser(body)
+        if(res.status === 201) readUser()
+    }
+    async function readUser(){
+        let res = await fetch('http://localhost:3000/users')
+        let data = await res.json()
+        console.log(data)
+        setUser(data)
+    }
+    function deleteUser(){}
+    function modifyUser(){}
+    // readUser()
+
+    useEffect(()=>{
+        readUser()
+    },[])
 
     return(
         <div className="container text-center">
             <h1 className='text-primary text-center display-2 mb-3'>User Management</h1>
             <div className="row text-center" >
                 <div className='col md-6'>
-                    <UserForm/>
+                    <UserForm createUser = {createUser}/>
                 </div>
                 <div className='col md-6'>
-                    <UserList/>
+                    <UserList deleteUser = {deleteUser} readUser = {readUser} modifyUser = {modifyUser} User = {User} />
                 </div>
             </div>
         </div>
